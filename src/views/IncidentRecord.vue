44e27238a5ef5c938bfaa186eb1ff58fc22b13e2
haoy-Li <84771527+haoy-Li@users.noreply.github.com>
2023-04-11 16:18:15 +0800

    基本完成事件记录页面（代码中不包含表格信息）

--- src/views/IncidentRecord.vue
+++ src/views/IncidentRecord.vue
@@ -5,7 +5,7 @@
     </el-card>
     <el-card class="Retrieval">
       <h1>信息检索</h1>
-      <el-form ref="InidentFormRef" :model="IncidentForm" :rules="IncidentFormRules" label-width="100px" class="Incident_form">
+      <el-form ref="InidentFormRef" :model="IncidentForm" :rules="IncidentFormRules" label-width="120px" class="Incident_form">
         
         <el-row>
         <el-col :span="8">
@@ -17,67 +17,85 @@
         
         
           <el-col :span="8">
-        <el-form-item prop="AccountType" label="账号类型">
+        <el-form-item prop="IncidentType" label="事件类型">
           
-          <el-select v-model="IncidentForm.AccountType" placeholder="请选择账号类型">
-            <el-option label="管理员" value="manager"></el-option>
-            <el-option label="教师" value="teacher"></el-option>
-            <el-option label="家长" value="parent"></el-option>
+          <el-select v-model="IncidentForm.IncidentType" placeholder="请选择事件类型">
+            <el-option label="自动上报" value="自动上报"></el-option>
+            <el-option label="家长上报" value="家长上报"></el-option>
           </el-select>
         </el-form-item>
         </el-col>
         <el-col :span="8">
         <el-form-item prop="Username" label="处理人">
-
           <el-input v-model="IncidentForm.Username" placeholder="请输入处理人"></el-input>
         </el-form-item>
         </el-col>
         </el-row>
         <el-row>
         <el-col :span="8">
-        <el-form-item prop="IncidentID" label="事件ID">
-          <el-input v-model="IncidentForm.IncidentID" placeholder="请输入事件ID"></el-input>
+        <el-form-item prop="IncidentID" label="事件编号">
+          <el-input v-model="IncidentForm.IncidentID" placeholder="请输入事件编号"></el-input>
         </el-form-item>
         </el-col>
         
           <el-col :span="8">
-        <el-form-item prop="Result" label="处理结果">
-          <el-select v-model="IncidentForm.Result" placeholder="请选择处理结果">
-            <el-option label="已处理" value="handled"></el-option>
-            <el-option label="正在处理" value="being handled"></el-option>
-            <el-option label="等待处理" value="will be handled"></el-option>
+        <el-form-item prop="Result" label="处理状态">
+          <el-select v-model="IncidentForm.Result" placeholder="请选择处理状态">
+            <el-option label="已处理" value="已处理"></el-option>
+            <el-option label="正在处理" value="正在处理"></el-option>
+            <el-option label="等待处理" value="待处理"></el-option>
+            <el-option label="无效事件" value="无效事件"></el-option>
            </el-select>
         </el-form-item>
         </el-col>
           <el-col :span="8">
-        <el-form-item prop="IsBully" label="是否霸凌">
-          <el-select v-model="IncidentForm.IsBully" placeholder="请选择是否霸凌">
+        <el-form-item prop="IsBully" label="是否存在霸凌">
+          <el-select v-model="IncidentForm.IsBully" placeholder="请选择是否存在霸凌">
             <el-option label="是" value="yes"></el-option>
             <el-option label="否" value="no"></el-option>
           </el-select>
         </el-form-item>
         </el-col>
-        </el-row>
         
+        </el-row>
+        <el-row>
+          <el-col :span="8">
+        <el-form-item prop="place" label="发生地点">
+          <el-select v-model="IncidentForm.IsBully" placeholder="请选择发生地点">
+            <el-option label="第一教学楼" value="第一教学楼"></el-option>
+            <el-option label="学生宿舍" value="学生宿舍"></el-option>
+          </el-select>
+        </el-form-item>
+        </el-col>
+
+      <el-col :span="8">
+        <el-form-item prop="victim" label="受害者">
+          <el-input v-model="IncidentForm.Username" placeholder="请输入受害者"></el-input>
+        </el-form-item>
+        </el-col>
+
         <el-form-item class="btns">
-          <el-button type="primary" @click="retrieval">检索</el-button>
+          <el-button type="primary" @click="retrieval">检索</el-button> <!--待增加检索方法-->
           <el-button type="info" @click="resetInidentForm">重置</el-button>
         </el-form-item>
+        </el-row>
       </el-form>
-    </el-card>
- <el-card class="ADD">
-    <el-row>
-        <el-button type="primary" plain @click="dialogVisible = true">新增</el-button>
-    </el-row>
       
-
+    </el-card>
+ <el-card class="Table">
+    <el-row>
+        <el-button type="primary" plain @click="download">下载</el-button> <!--待增加下载方法-->
+    </el-row>
+    
+      
+<!--
     <el-dialog
             title="添加事件"
             :visible.sync="dialogVisible"
             width="40%"
             >
  
-        <!--弹出的对话框的内容-->
+        //弹出的对话框的内容
         <el-form ref="addFormRef" :model="addForm" :rules="addFormRules" label-width="80px">
         <el-form-item prop="Time" label="日期">
           <el-date-picker v-model="addForm.Time" placeholder="请选择日期" value-format="yyyy-MM-dd"></el-date-picker>
@@ -118,65 +136,115 @@
             </el-form-item>
         </el-form>
     </el-dialog>
-
+-->
 
 
 <!--表格主体-->
+<div>
 <el-table       border
-                :data="tableData"
+                :data="tableData.slice((currentPage-1)*pageSize,currentPage*pageSize)"
                 style="width: 100%"
                 :row-class-name="tableRowClassName"
                 @selection-change="handleSelectionChange"
                  max-height="840" >
-
+        <el-table-column label="序号" width="80px" align='center'>
+        <template slot-scope="scope">
+          <span>{{ scope.$index +1 }}</span>
+        </template>
+        </el-table-column>
+        <!--
         <el-table-column type="selection" width="55" align="center">
         </el-table-column>
-        <el-table-column prop="Time" width="200" label="日期" align="center" sortable=""> </el-table-column>
-       <el-table-column prop="AccountType" width="200" label="账号类型" align="center">
+        -->
+       <el-table-column prop="IncidentID" width="140" label="事件编号" align="center" sortable="">
        </el-table-column>
-       <el-table-column prop="Username" width="200" label="处理人" align='center'>
+        <el-table-column prop="Time" width="180" label="上报时间" align="center" sortable=""> 
+          <template slot-scope="scope">
+          <span v-if="!scope.row.isEgdit">{{scope.row.Time}}</span>
+          <el-input v-if="scope.row.isEgdit" v-model="scope.row.Time"></el-input>
+        </template>
+        </el-table-column>
+        
+       <el-table-column prop="IncidentType" width="150" label="事件类型" align="center">
+        <template slot-scope="scope">
+          <span v-if="!scope.row.isEgdit">{{scope.row.IncidentType}}</span>
+          <el-select v-if="scope.row.isEgdit" v-model="scope.row.IncidentType" placeholder="请选择事件类型">
+            <el-option label="自动上报" value="自动上报"></el-option>
+            <el-option label="家长上报" value="家长上报"></el-option>
+           </el-select>
+        </template>
+       </el-table-column>
+
+       <el-table-column prop="Username" width="100" label="处理人" align='center'>
         <template slot-scope="scope">
           <span v-if="!scope.row.isEgdit">{{scope.row.Username}}</span>
           <el-input v-if="scope.row.isEgdit" v-model="scope.row.Username"></el-input>
         </template>
       </el-table-column>
-       <el-table-column prop="IncidentID" width="200" label="处理事件ID" align="center" sortable="">
-       </el-table-column>
 
-       <el-table-column prop="Result" width="200" label="处理结果" align="center">
+       <el-table-column prop="Result" width="140" label="处理状态" align="center">
         <template slot-scope="scope">
           <span v-if="!scope.row.isEgdit">{{scope.row.Result}}</span>
-          <el-select v-if="scope.row.isEgdit" v-model="scope.row.Result" placeholder="请选择处理结果">
+          <el-select v-if="scope.row.isEgdit" v-model="scope.row.Result" placeholder="请选择处理状态">
             <el-option label="已处理" value="已处理"></el-option>
             <el-option label="正在处理" value="正在处理"></el-option>
             <el-option label="等待处理" value="待处理"></el-option>
+            <el-option label="无效事件" value="无效事件"></el-option>
            </el-select>
         </template>
       </el-table-column>
-       <el-table-column prop="IsBully" width="200" label="是否霸凌" align="center">
+
+       <el-table-column prop="IsBully" width="140" label="是否存在霸凌" align="center">
         <template slot-scope="scope">
           <span v-if="!scope.row.isEgdit">{{scope.row.IsBully}}</span>
-          <el-select v-if="scope.row.isEgdit" v-model="scope.row.IsBully" placeholder="请选择是否霸凌">
+          <el-select v-if="scope.row.isEgdit" v-model="scope.row.IsBully" placeholder="请选择是否存在霸凌">
             <el-option label="是" value="是"></el-option>
             <el-option label="否" value="否"></el-option>
            </el-select>
         </template>
        </el-table-column>
-       <el-table-column prop="Details" width="500" label="事件详情" align="center">
+
+      <el-table-column prop="place" width="140" label="发生地点" align="center">
+        <template slot-scope="scope">
+          <span v-if="!scope.row.isEgdit">{{scope.row.place}}</span>
+          <el-select v-if="scope.row.isEgdit" v-model="scope.row.place" placeholder="请选择发生地点">
+            <el-option label="第一教学楼" value="第一教学楼"></el-option>
+            <el-option label="学生宿舍" value="学生宿舍"></el-option>
+           </el-select>
+        </template>
+       </el-table-column>
+
+       <el-table-column prop="victim" width="100" label="受害者" align="center">
+        <template slot-scope="scope">
+          <span v-if="!scope.row.isEgdit">{{scope.row.victim}}</span>
+          <el-input v-if="scope.row.isEgdit" v-model="scope.row.victim"></el-input>
+        </template>
+       </el-table-column>
+
+       <el-table-column prop="Details" width="1068" label="详细信息" align="left">
         <template slot-scope="scope">
           <span v-if="!scope.row.isEgdit">{{scope.row.Details}}</span>
           <el-input v-if="scope.row.isEgdit" v-model="scope.row.Details"></el-input>
         </template>
        </el-table-column>
-       <el-table-column prop="operation" width="500" label="操作" align="center">
+
+       <el-table-column prop="operation" width="100" label="操作" align="center">
        <template slot-scope="scope">
           <el-button v-if="!scope.row.isEgdit" type="primary" size="small" @click='edit(scope.$index,scope.row)' icon="el-icon-edit" circle></el-button>
           <el-button v-if="scope.row.isEgdit" type="success" size="small" @click='editSuccess(scope.$index,scope.row)' icon="el-icon-check" circle></el-button>
-          <el-button @click.native.prevent="deleteItem(scope.$index, list)" type="danger" size="small" icon="el-icon-delete" circle></el-button>
         </template>
-
        </el-table-column>
      </el-table>
+     <div class="block" style="margin-top:15px;">
+            <el-pagination align='center' @size-change="handleSizeChange" @current-change="handleCurrentChange" 
+            :current-page="currentPage" 
+            :page-sizes="[13,1,5,10]" 
+            :page-size="pageSize" 
+            layout="total, sizes, prev, pager, next, jumper" 
+            :total="45">
+            </el-pagination>
+        </div>
+        </div>
      </el-card>
 </div>
 
@@ -189,92 +257,7 @@ export default {
   data() {
     
     return {
-      dialogVisible: false,//对话框是否可见
-      isCollapse: false,  // 侧边栏是否折叠
-      IncidentForm: {//检索框参数
-        Time: '',
-        AccountType: '',
-        Username: '',
-        IncidentID: '',
-        Result: '',
-        IsBully: ''
-      },
-      addForm: {//对话框参数
-        Time: '',
-        AccountType: '',
-        Username: '',
-        IncidentID: '',
-        Result: '',
-        IsBully: '',
-        Details:'',
-      },
-      tableData: [{//表格数据
-        Time: '2021-06-03',
-        AccountType: '管理员',
-        Username: '李昊洋',
-        IncidentID: '125896324',
-        Result: '正在处理',
-        IsBully: '是',
-        Details:'',
-      },
-      {
-        Time: '2023-02-23',
-        AccountType: '教师',
-        Username: '李昊洋',
-        IncidentID: '125896348',
-        Result: '已处理',
-        IsBully: '否',
-        Details:'',
-      },
-      {
-        Time: '2022-07-15',
-        AccountType: '家长',
-        Username: '李昊洋',
-        IncidentID: '125896365',
-        Result: '待处理',
-        IsBully: '否',
-        Details:'',
-      },
       
-      ],
-      // 检索规则
-      IncidentFormRules: {
-
-
-        IncidentID: [
-          { required: false, message: '请输入ID', trigger: 'blur' },
-          { min: 1, max: 10, message: '请输入正确的ID', trigger: 'blur' }
-        ],
-        Username: [
-          { required: false, message: '请输入处理人', trigger: 'blur' },
-          { min: 2, max: 20, message: '请输入正确的处理人姓名', trigger: 'blur' }
-        ]
-      },
-      //添加规则
-      addFormRules: {
-        Time: [
-          { required: true, message: '请选择时间', trigger: 'blur' },
-        ],
-        AccountType: [
-          { required: true, message: '请选择账号类型', trigger: 'blur' },
-        ],
-        Username: [
-          { required: true, message: '请输入处理人', trigger: 'blur' },
-        ],
-        IncidentID: [
-          { required: true, message: '请输入事件ID', trigger: 'blur' },
-          { min: 1, max: 10, message: '请输入正确的ID', trigger: 'blur' }
-        ],
-        Result: [
-          { required: true, message: '请选择处理结果', trigger: 'blur' },
-        ],
-        IsBully: [
-          { required: true, message: '请选择是否霸凌', trigger: 'blur' },
-        ],
-        Details: [
-          { required: false, message: '请输入事件详情', trigger: 'blur' },
-        ]
-      }
     }
   },
   watch: {
@@ -297,9 +280,6 @@ export default {
     }
   },
   methods: {
-    deleteItem(index, list) {
-      list.splice(index, 1);
-    },
     //编辑数据
     edit(index, row) {
       this.$set(row, 'isEgdit', true)
@@ -309,7 +289,17 @@ export default {
       this.$set(row, 'isEgdit', false)
     },
 
-
+    //每页条数改变时触发 选择一页显示多少行
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      this.currentPage = 1;
+      this.pageSize = val;
+          },
+    //当前页改变时触发 跳转其他页
+    handleCurrentChange(val) {
+    console.log(`当前页: ${val}`);
+    this.currentPage = val;
+      },
     // 重置输入
     resetInidentForm() {
       // console.log(this);
@@ -317,21 +307,24 @@ export default {
       this.$refs.InidentFormRef.resetFields()
     },
     //添加数据方法
-    addIncident(){
-      var newValue = {
-        Time: this.addForm.Time,
-        AccountType: this.addForm.AccountType,
-        Username: this.addForm.Username,
-        IncidentID: this.addForm.IncidentID,
-        Result: this.addForm.Result,
-        IsBully: this.addForm.IsBully,
-        Details: this.addForm.Details,
-      }
-      this.tableData.push(newValue)
-      this.$refs.addFormRef.resetFields()
-      this.dialogVisible= false
-    },
-
+    //addIncident(){
+      //var newValue = {
+        //Time: this.addForm.Time,
+        //AccountType: this.addForm.AccountType,
+        //Username: this.addForm.Username,
+        //IncidentID: this.addForm.IncidentID,
+        //Result: this.addForm.Result,
+        //IsBully: this.addForm.IsBully,
+        //Details: this.addForm.Details,
+      //}
+      //this.tableData.push(newValue)
+      //his.$refs.addFormRef.resetFields()
+      //this.dialogVisible= false
+    //},
+    //
+    retrieval(){
+      console.log(this.I)
+    }
   },
   mounted() {
     this.isCollapse = this.$store.state.isCollapse;
@@ -361,7 +354,7 @@ export default {
   position: relative;
   margin-top: 20px;
   bottom: 50;
-  width: 40%;
+  width: 50%;
   padding: 0px;
   box-sizing: border-box;
 }
